b35fbc77c644a233a55c1d850dd2ec8309d838d7
Logan Yamamoto <lkyamamo@usc.edu>
2022-12-02 09:43:47 -0800

    finished hash function

--- hash.h
+++ hash.h
@@ -20,15 +20,91 @@ struct MyStringHash {
     HASH_INDEX_T operator()(const std::string& k) const
     {
         // Add your code here
+        size_t kLength = k.length();
+        HASH_INDEX_T w[5] = {0,0,0,0,0};
 
+        //setting substrings
 
+        //less than or equal to 6 characters long
+        if(kLength <= 6)
+        {
+            char a[6];
+            //set the characters that exist
+            for(size_t i = 0; i < kLength; ++i)
+            {
+                a[i] = k[kLength - 1 - i];
+            }
+            //set the rest to something outside of generating range;
+            for(size_t i = kLength; i < 6; ++i)
+            {
+                a[i] = '-';
+            }
+
+            //adding up the values
+            for(size_t i = 0; i < 6; ++i)
+            {
+                std::cout<< letterDigitToNumber(a[i]) << std::endl;
+                w[4] += pow(36,i) * letterDigitToNumber(a[i]);
+            }
+            
+            std::cout << "w[4] = " << w[4] << std::endl;
+            return w[4] * rValues[4];
+        }
+        //length greater than 6 characters
+        else
+        {
+            std::string a[6];
+
+            int counter = 4;
+            for(size_t i = kLength - 1; i+12 >= 12; i -= 6)
+            {
+                //adding the substring to character array to be prosessed 
+                char b[6] = {'-','-', '-', '-', '-', '-'};
+                size_t j = 0;
+                for(; (j < 6) && (i - j + 6 >= 6); ++j)
+                {
+                    b[j] = k[i-j];
+                }
+                //set the rest to something outside of generating range;
+                for(; j < 6; ++j)
+                {
+                    
+                    b[j] = '-';
+                }
+
+                //adding up the values
+                for(int k = 0; k < 6; ++k)
+                {
+                    //std::cout<< letterDigitToNumber(b[j]) << std::endl;
+                    w[counter] += pow(36,k) * letterDigitToNumber(b[k]);
+                }
+                --counter;
+            }
+
+            HASH_INDEX_T temp = 0;
+            for(int i = 0; i < 5; ++i)
+            {
+                std::cout << "w[" << i << "] = " << w[i] << std::endl;
+
+                //multiply by correlated rValue and add to hash total
+                temp += rValues[i] * w[i];
+            }
+            return temp;
+        }
     }
 
     // A likely helper function is to convert a-z,0-9 to an integral value 0-35
     HASH_INDEX_T letterDigitToNumber(char letter) const
     {
         // Add code here or delete this helper function if you do not want it
+        letter = tolower(letter);
 
+        //character is a letter
+        if((int) letter >= 97 && (int) letter <= 122) return ((int) letter) - 97;
+        //character is a number
+        else if((int) letter >= 48 && (int) letter <= 57) return ((int) letter) - 22;
+        //character is neither
+        else return 0;
     }
 
     // Code to generate the random R values
